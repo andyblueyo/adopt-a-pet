f6bac72f34feb2f58652aff2ec666028f7a2c21e
dbabbs <dbabbs@uw.edu>
2017-05-29 12:38:29 -0700

    functions complete

--- scripts.sql
+++ scripts.sql
@@ -32,7 +32,41 @@ ALTER TABLE ANIMAL_TYPE
 
 -- 2. Adopt-A-Pet will not accept any cats or dogs over 100 pounds
 
+CREATE FUNCTION NoLargeAnimals (@Weight VARCHAR(50))
+RETURNS INT
+AS
+BEGIN
+  DECLARE @RET INT
+    SET @RET = (SELECT CASE WHEN @Weight < 100
+                  THEN 0 ELSE 1 END)
+    FROM MEASUREMENTS
+    WHERE Weight = @Weight
+  RETURN @RET
+END;
+GO
+
+ALTER TABLE ANIMAL_TYPE
+  ADD CONSTRAINT chkAnimalWeight
+  CHECK (dbo.NoLargeAnimals(Weight) = 0);
+
 -- 3. ShipDate must come after OrderDate
 
+CREATE FUNCTION OrderBeforeShip (@OrderDate DATE, @ShipDate DATE))
+RETURNS INT
+AS
+BEGIN
+  DECLARE @RET INT
+    SET @RET = (SELECT CASE WHEN @OrderDate < @ShipDate
+                  THEN 0 ELSE 1 END)
+    FROM ORDER
+    WHERE OrderDate = @OrderDate
+    AND ShipDate = @ShipDate
+  RETURN @RET
+END;
+GO
+
+ALTER TABLE ORDER
+  ADD CONSTRAINT chkDates
+  CHECK (dbo.OrderBeforeShip(OrderDate, ShipDate) = 0);
 
 --*****************COMPUTED COLUMNS*****************
